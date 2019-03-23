fc7ef3e68ee9eb6551612ead173ce145f40710ec
manuelgomezsuarez@gmail.com <manuelgomezsuarez@gmail.com>
2019-03-23 21:24:48 +0100

    wsgi

--- apiMotoGP/wsgi.py
+++ apiMotoGP/wsgi.py
@@ -1,19 +1,14 @@
-"""
-WSGI config for apiMotoGP project.
 
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
-
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
-"""
+# +++++++++++ DJANGO +++++++++++
+# To use your own django app use code like this:
 import os
+import sys
+#
+## assuming your django settings file is at '/home/simplicat/mysite/mysite/settings.py'
+## and your manage.py is is at '/home/simplicat/mysite/manage.py'
+path = '/home/manugomez95/TFG-ManuAlvaro/apiMotoGP/apiMotoGP'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apiMotoGP.settings")
 
@@ -22,7 +17,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apiMotoGP.settings")
 # setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
