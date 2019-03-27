84b0bdd1c67fdcd23e345ec462b1897d770d795c
manuelgomezsuarez@gmail.com <manuelgomezsuarez@gmail.com>
2019-03-27 21:38:22 +0100

    test Assets

--- apiMotoGP/wsgi.py
+++ apiMotoGP/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-
+from dj_static import Cling
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apiMotoGP.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
