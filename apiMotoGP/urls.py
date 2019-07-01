a4e615b24ce0c883f4fe1b6b328910df880f4962
manuelgomezsuarez@gmail.com <manuelgomezsuarez@gmail.com>
2019-07-02 00:09:33 +0200

    despliegue

--- apiMotoGP/urls.py
+++ apiMotoGP/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
-from rest_framework.documentation import include_docs_urls
-
 from rest_framework import permissions
 
 
@@ -11,6 +9,5 @@ from rest_framework import permissions
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('app.urls')),
-    url(r'^docs/', schema_view),
 
 ]
